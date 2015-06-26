967a11e642693448cebd0ad49ad8078014cefcf3
Aaron <aaron.boesch@woolpert.com>
2015-06-26 15:47:26 -0400

    initial commit

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelPram)
+        {
+            this.kernel = kernelPram;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+
+    }
+}
